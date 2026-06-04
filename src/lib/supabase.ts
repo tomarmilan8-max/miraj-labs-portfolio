31bf332617661b76d408b4ab94b13a6dcdc0c5f3
Miraj Labs <deploy@mirajlabs.com>
2026-06-04 03:27:22 +0000

    Fix Supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://ikfsgebpkdjulddsnuno.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlrg3NnZWJwa2RqdWxkZHNudW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NjIzMjYsImV4cCI6MjA5NjAzODMyNn0.3L_6hdSdUL1jy7NWrxxrL2hCxAoag79_DaWlPdN_yD8'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrZnNnZWJwa2RqdWxkZHNudW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NjIzMjYsImV4cCI6MjA5NjAzODMyNn0.3L_6hdSdUL1jy7NWrxxrL2hCxAoag79_DaWlPdN_yD8'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
